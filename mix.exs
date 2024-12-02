430bc1ba695bd9cc21a1dfa1b583364a203a4b1b
Aslak Johansen <aslakj@gmail.com>
2024-12-02 12:57:36 +0100

    Added some package configuration

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Cflat.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,6 +25,17 @@ defmodule Cflat.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+  
+  defp description() do
+    "Implements the C♭ language (which aims to be a subset of C#)."
+  end
+  
+  defp package() do
+    [
+      licenses: ["BSD-3-Clause"],
+      links: %{"GitHub" => "https://github.com/aslakjohansen/cflat"}
     ]
   end
 end

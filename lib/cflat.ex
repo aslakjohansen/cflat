d2c12983572a68281abb33aa56fa12c28e8340e6
Aslak Johansen <aslakj@gmail.com>
2024-12-02 12:33:44 +0100

    Initial commit

---
+++ lib/cflat.ex
@@ -0,0 +1,18 @@
+defmodule Cflat do
+  @moduledoc """
+  Documentation for `Cflat`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cflat.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cflat_test.exs
@@ -0,0 +1,8 @@
+defmodule CflatTest do
+  use ExUnit.Case
+  doctest Cflat
+
+  test "greets the world" do
+    assert Cflat.hello() == :world
+  end
+end
